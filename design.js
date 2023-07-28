5be0aa8d785844ce96fcf5f4a5515bce43cbc17b
DeekshithaDamma <135822283+DeekshithaDamma@users.noreply.github.com>
2023-07-28 16:04:35 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+<div>
+    <h1>Mother Teresa</h1>
+    <img src="https://mycatholic.life/wp-content/uploads/2019/07/Saint-mother-Teresa-at-Marquette-University.jpg"height="50%"width="50%"></img>
+    <ul>
+        <li>Born: 26 August 1910, Skopje, North Macedonia</li>
+        <li>Died: 5 September 1997, Kolkata</li>
+        <li>Organizations founded: Missionaries of Charity, MORE</li>
+        <li>In 2016, Mother Teresa was canonised by the Roman Catholic Church as Saint Teresa.</li>
+    </ul>
+    <h1>Awards given to Mother Teresa</h1>
+    <ul>
+        <li>The first Pope John XXIII Peace Prize. (1971)</li>
+        <li>Kennedy Prize (1971)</li>
+        <li>The Nehru Prize –“for the promotion of international peace and understanding”(1972)</li>
+        <li>Albert Schweitzer International Prize (1975),</li>
+        <li>The Nobel Peace Prize (1979)</li>
+    </ul>
+   <h1>About Mother Teresa Charities and Missionaries</h1>
+   <p>Teresa sought and obtained Vatican permission to start a diocesan congregation, which would become the Missionaries of Charity, after working on the streets of Calcutta for two years. "The poor, the naked, the homeless, the crippled, the blind, the lepers," Teresa explained, "all those people who feel unwanted, unloved, and uncared for in society, people who have become a burden to society and are shunned by others." In Calcutta, it started as a small order of 12 members.</p>
+   <p>More than 4,000 nuns ran orphanages, AIDS hospices, and charity centres on all six continents by 2006, caring for refugees, the blind, disabled, elderly, alcoholics, the sick, homeless, and victims of floods, epidemics, and famine</p>
+                                         <h2>**Not all of us can do great things. But we can do small things with great love**</h2>
+</div>
+,document.getElementById("content"));
